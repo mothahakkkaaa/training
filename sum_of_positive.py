43f6a259ca9f84e7123bae73e62551b45be4917f
mothahakkkaaa <slavataranec@gmail.com>
2022-09-17 15:26:25 +0300

    sum_of_positive task performance

---
+++ sum_of_positive.py
@@ -0,0 +1,16 @@
+"""You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+"""
+def positive_sum(arr):
+    summ = 0
+    for i in arr:
+        if i > 0:
+            summ += i
+    return summ
+
+
+massive_attack = [1,-4,7,12]
+print(positive_sum(massive_attack))
